1843cd040b9743fe8d3ae756197b758aae56ccc9
Anastasiia <a.khrypko.work@gmail.com>
2025-01-22 22:03:04 +0100

    localStorage added

--- src/contexts/Query.context.jsx
+++ src/contexts/Query.context.jsx
@@ -1,34 +1,47 @@
 import { createContext, useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
 export const QueryContext = createContext();
 
 export const QueryProvider = ({ children }) => {
-    const [query, setQuery] = useState("fish");
+    const [query, setQuery] = useState(() => {
+        return localStorage.getItem("query") || "fish";
+    });
     const [recipes, setRecipes] = useState([]);
-
-    console.log(query);
+    const [recipeLabel, setRecipeLabel] = useState(() => {
+        return localStorage.getItem("recipeLabel") || null;
+    });
 
     const changeQuery = (newQuery) => {
         setQuery(newQuery);
+        localStorage.setItem("query", newQuery);
     };
 
-    const apiId = import.meta.env.VITE_API_ID;
-    const apiKey = import.meta.env.VITE_API_KEY;
-  
+    const changeRecipeLabel = (newRecipeLabel) => {
+        setRecipeLabel(newRecipeLabel);
+        localStorage.setItem("recipeLabel", newRecipeLabel);
+    };
+
     useEffect(() => {
+        const apiId = import.meta.env.VITE_API_ID;
+        const apiKey = import.meta.env.VITE_API_KEY;
+
         const getRecipes = async () => {
-            const response = await fetch(
-              `https://api.edamam.com/search?q=${query}&app_id=${apiId}&app_key=${apiKey}`
-            );
+            const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${apiId}&app_key=${apiKey}`);
             const data = await response.json();
             setRecipes(data.hits);
-          };
-          getRecipes();
+        };
+
+        getRecipes();
     }, [query]);
 
     return (
-        <QueryContext.Provider value={{ query, changeQuery, recipes }}>
+        <QueryContext.Provider value={{ query, changeQuery, recipes, recipeLabel, changeRecipeLabel }}>
             {children}
         </QueryContext.Provider>
     );
-}
+};
+
+QueryProvider.propTypes = {
+    children: PropTypes.object,
+};
