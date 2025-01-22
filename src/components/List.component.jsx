f6221fa78240f88950895bc5b3777e436c3ef036
Anastasiia <a.khrypko.work@gmail.com>
2025-01-22 22:00:45 +0100

    scroll to top added

--- src/components/List.component.jsx
+++ src/components/List.component.jsx
@@ -1,17 +1,23 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { QueryContext } from "../contexts/Query.context";
 import RecipeCard from "./RecipeCard.component";
 
 function List() {
     const { query, recipes } = useContext(QueryContext);
 
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <>
             <h2>Recipes with {query}:</h2>
             <ul>
-                {recipes.map((recipeObj, index) => <RecipeCard key={index} recipeObj={recipeObj} />)}
+                {recipes.map((recipeObj, index) => (
+                    <RecipeCard key={index} recipeObj={recipeObj} />
+                ))}
             </ul>
-        </> 
+        </>
     );
 }
 
