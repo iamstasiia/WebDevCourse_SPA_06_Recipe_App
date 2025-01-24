b3ccdb73caee062b079d598673a0a6430d2651c4
Anastasiia <a.khrypko.work@gmail.com>
2025-01-24 21:59:13 +0100

    attr name added

--- src/components/Search.component.jsx
+++ src/components/Search.component.jsx
@@ -2,27 +2,30 @@ import { useContext, useState } from "react";
 import { QueryContext } from "../contexts/Query.context";
 
 function Search() {
-    const [search, setSearch] = useState('');
+    const [search, setSearch] = useState("");
     const onSubmitHandler = (event) => {
         event.preventDefault();
         if (search.length !== 0) {
             changeQuery(search);
-            setSearch('');
+            setSearch("");
         }
-    }
+    };
 
     const { changeQuery } = useContext(QueryContext);
 
     return (
         <form onSubmit={onSubmitHandler} className="search-form">
             <input
-              className="search-bar"
-              type="text"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              placeholder="Enter the product"
+                className="search-bar"
+                type="text"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Enter the product"
+                name="search-bar"
             />
-            <button className="search-btn" type="submit"><i className="fa-solid fa-magnifying-glass"></i></button>
+            <button className="search-btn" type="submit">
+                <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
         </form>
     );
 }
